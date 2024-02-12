54c9feb729a8e2aadf8b9ac321921678de4ab81f
Yago Elon <yago_elon@hotmail.com>
2024-02-12 00:48:43 -0300

    Concatenação add

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,13 +1,13 @@
 //alert("Boas vindas ao jogo!");
-let numeroSecreto = 29;
+let numeroSecreto = 5;
 let chute = prompt("Escolha um número entre 1 e 30");
 console.log(numeroSecreto);
 
 
 if (numeroSecreto == chute) {
-    alert("Acertou");
-    console.log("Acertou");
+    alert(`Acertou! O número secreto era ${numeroSecreto}`); //método com crase, cifrao e chaves
+    console.log("Acertou! O número secreto era: " + numeroSecreto); //método com aspas e +
 } else {
-    alert("Errou");
-    console.log("Errou");
+    alert("Errou! O número secreto era: " + numeroSecreto);
+    console.log("Errou! O número secreto era: " + numeroSecreto);
 };
