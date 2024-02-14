00730e3bd19db49c640d93632f5ada47540c9541
Yago Elon <yago_elon@hotmail.com>
2024-02-14 01:34:02 -0300

    Novas alterações com while

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,13 +1,22 @@
-//alert("Boas vindas ao jogo!");
+alert("Boas vindas ao jogo!");
 let numeroSecreto = 5;
-let chute = prompt("Escolha um número entre 1 e 30");
+var tentativas = 1;
 console.log(numeroSecreto);
 
-
-if (numeroSecreto == chute) {
-    alert(`Acertou! O número secreto era ${numeroSecreto}`); //método com crase, cifrao e chaves
-    console.log("Acertou! O número secreto era: " + numeroSecreto); //método com aspas e +
-} else {
-    alert("Errou! O número secreto era: " + numeroSecreto);
-    console.log("Errou! O número secreto era: " + numeroSecreto);
-};
+while (chute != numeroSecreto) {
+    var chute = prompt("Escolha um número entre 1 e 30");
+    if (numeroSecreto == chute) {
+        alert(`Acertou! O número secreto era ${numeroSecreto}, após efetuar ${tentativas} tentativas`); //método com crase, cifrao e chaves
+        console.log("Acertou! O número secreto era: " + numeroSecreto); //método com aspas e +
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`Errou! O número secreto é menor que ${chute}`);
+        } else {
+            alert(`Errou! O número secreto é maior que ${chute}`);
+        }
+        /*alert("Errou! O número secreto era: " + numeroSecreto);
+        console.log("Errou! O número secreto era: " + numeroSecreto);*/
+        //tentativas = tentativas + 1;
+        tentativas ++;
+    };
+}
--- logica-js-projeto_inicial/pratica.js
+++ logica-js-projeto_inicial/pratica.js
@@ -1,4 +1,4 @@
-alert("Boas vindas ao nosso site!");
+/*alert("Boas vindas ao nosso site!");
 
 //var nome = "Lua";
 //var idade = 25;
@@ -18,3 +18,81 @@ if (idade >= 18) {
     alert("NÃO pode tirar a habilitação!");
     console.log("NÃO pode tirar a habilitação!")
 };
+*/
+
+/*
+//1
+var diaSemana = prompt("Qual o dia da semana hoje?");
+
+if (diaSemana == "Sábado" || diaSemana == "Domingo") {
+    alert("Bom final de semana!");
+} else if (diaSemana == "Segunda" || diaSemana == "Terça" || diaSemana == "Quarta" || diaSemana == "Quinta" || diaSemana == "Sexta") {
+    alert("Boa semana!");
+} else {
+    alert("NOME DA SEMANA INVÁLIDO, TENTE NOVAMENTE");
+};
+//2
+var numero = prompt("Digite um número:");
+
+if (numero >= 0) {
+    alert("O número digitado é positivo");
+} else {
+    alert("O número digitado é negativo");
+};
+//3
+var numeroJogo = prompt("Digite sua pontuação:");
+
+if (numeroJogo >= 100) {
+    alert("Parabéns, você venceu!");
+} else {
+    alert("Tente novamente para ganhar.");
+};
+//4
+var saldo = prompt("Digite seu saldo atual");
+alert(`Seu saldo da conta é R$${saldo}`);
+//5
+var nomeUser = prompt("Digite seu nome:");
+alert(`Boas-vindas ${nomeUser}`);
+*/
+
+/*let qtdNumeros = prompt('Digite a quantidade de números para o cálculo da média:');
+let soma = 0;
+let contador = 0;
+
+while(contador != qtdNumeros){
+    let numero = parseInt(prompt('Digite o numero:'));
+    soma += numero;
+    contador ++
+}
+
+let media = soma / qtdNumeros;
+
+console.log(media);*/
+
+/*contador=0
+
+while(contador < 10){
+    contador++
+    alert(contador);
+};
+
+contador=11
+
+while(contador > 0){
+    contador--
+    alert(contador);
+};
+
+contador=prompt("Digite um número:")
+
+while(contador > 0){
+    contador--
+    console.log(contador);
+};
+
+contador=0
+numero=prompt("Digite um número:")
+while(contador < numero){
+    contador++
+    console.log(contador);
+};*/
